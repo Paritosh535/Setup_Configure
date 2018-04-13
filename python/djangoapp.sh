5540efd90f80cc97218563c679d01388796037bb
Paritosh Yadav <paritoshy535@gmail.com>
2018-04-13 17:49:32 +0530

    django modify

--- python/djangoapp.sh
+++ python/djangoapp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo -e "\033[1m\033[36m Enter Project Name First"
 read project_name
-cd pwd
 cd $project_name
-mkdir pp
+cd Dev/$project_name/src
 
-# echo -e " \033[1m\033[32mEnter Project App Name"
-# read app_name
+echo -e " \033[1m\033[32mEnter Project App Name"
+read app_name
+python manage.py startapp $app_name 
+
 # touch $app_name
 # python manage.py startapp $app_name
 # cd $app_name && mkdir api && cd api && touch serializer.py urls.py views.py
