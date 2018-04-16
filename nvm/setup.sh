ef643be0a72bbbaebe4d84c7864a9e5ca65b63ac
Paritosh Yadav <paritoshy535@gmail.com>
2018-04-16 12:17:40 +0530

    node

---
+++ nvm/setup.sh
@@ -0,0 +1,16 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+nvm --version
+echo -e "1=> latest \n 2=> lts"
+read option
+if 
+
+nvm install node
+nvm use node
+nvm install --lts
+nvm use --lts
+
+Install the latest version with nvm install node
+Use the latest version with nvm use node
+Install the latest LTS version with nvm install --lts
+Use the latest LTS verison with nvm use --lts
