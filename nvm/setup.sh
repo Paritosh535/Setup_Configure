b7502517f18933193d2e89157d907c95cae78130
Paritosh Yadav <paritoshy535@gmail.com>
2018-04-16 12:29:04 +0530

    nvm operation

--- nvm/setup.sh
+++ nvm/setup.sh
@@ -1,16 +1,23 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
 nvm --version
-echo -e "1=> latest \n 2=> lts"
+echo -e "1=> latest \n2=> lts"
 read option
-if 
+if [ $option -eq 1 ]
+then
+    echo "if"
+#nvm install node
+#nvm use node
+elif [ $option -eq 2 ]
+then
+    echo "elseif"
+#nvm install --lts
+#nvm use --lts
+else
+    echo "else"
+fi
 
-nvm install node
-nvm use node
-nvm install --lts
-nvm use --lts
-
-Install the latest version with nvm install node
-Use the latest version with nvm use node
-Install the latest LTS version with nvm install --lts
-Use the latest LTS verison with nvm use --lts
+#Install the latest version with nvm install node
+#Use the latest version with nvm use node
+#Install the latest LTS version with nvm install --lts
+#Use the latest LTS verison with nvm use --lts
