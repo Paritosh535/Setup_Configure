4fe0fd85e3a451b975bd22451013af455e6bd926
paritosh535 <paritoshy535@gmail.com>
2018-04-22 10:41:44 +0530

    dnvm setuo

--- docker/docker.sh
+++ docker/docker.sh
@@ -27,9 +27,12 @@ $ sudo apt-get install docker-ce
 
 echo "NOTE INSTSLL Latest version from site MACHINE/COMPOSE"
 echo "Docker Machine install"
-curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && \
-sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 
 echo "Docker compose"
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+echo "Need in AWS instance"
+sudo chmod +x /usr/local/bin/docker-compose
